1c0e83eb7d3f9acf089f2e2b578e53f41141b894
Scyn4pse <canquechiara76@gmail.com>
2026-02-07 18:24:07 +0800

    env push

---
+++ env.ts
@@ -0,0 +1,8 @@
+// AsyncStorage Keys
+export const STORAGE_KEYS = {
+  SOOTHERA_FOLDER_URI: 'soothera_folder_uri',
+  INVOICE_REGISTRY: 'soothera_invoice_registry',
+} as const;
+
+// Mapbox Access Token
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWppd25sIiwiYSI6ImNtMzhsaHFzNTB0dmsyaXE1enV5aXNrbjcifQ.MKG4wR3aMbdde0oisZLH7g';
