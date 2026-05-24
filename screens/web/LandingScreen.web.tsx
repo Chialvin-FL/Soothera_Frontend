8b9e8c3f66b9527b9463e4f977a52798283a8ce0
ajiwnl <alvinjohnaranetz@gmail.com>
2026-05-24 13:13:59 +0800

    feat: add landing page with APK download support and update Expo dependencies

--- screens/web/LandingScreen.web.tsx
+++ screens/web/LandingScreen.web.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, Pressable, ImageBackground, Image } from 'react-native';
+import { View, ScrollView, Pressable, Image } from 'react-native';
 import { Text } from '@/components/Text';
 import { Ionicons } from '@expo/vector-icons';
 
